6c03a54f70ee8a75abbf3ee1619708e6ff0e519a
James <lalalili@gmail.com>
2018-01-15 13:19:09 +0800

    (Update) update css js url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,20 +6,20 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>{{ __('menu.company_name') }}</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/animate.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/animate.min.css" rel="stylesheet">
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/lightbox.css" rel="stylesheet">
     <link href="{{ url('css/main.css') }}" rel="stylesheet">
-    <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
-    <link href="css/responsive.css" rel="stylesheet">
+    <link id="css-preset" href="/css/presets/preset1.css" rel="stylesheet">
+    <link href="/css/responsive.css" rel="stylesheet">
 
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="/js/html5shiv.js"></script>
+    <script src="/js/respond.min.js"></script>
     <![endif]-->
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
     <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
